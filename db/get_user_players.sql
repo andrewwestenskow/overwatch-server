395057a4ade20c570ab094a3b61c6e964e715927
Andrew Westenskow <andrew.westenskow@gmail.com>
2020-07-22 13:43:54 -0600

    IDK

--- db/get_user_players.sql
+++ db/get_user_players.sql
@@ -1,3 +1,3 @@
-SELECT pl.id, pl.name, pt.name as platform FROM players pl
+SELECT pl.id, pl.name, pl.private, pl.portrait, pl.tank_sr, pl.damage_sr, pl.support_sr, pt.name as platform FROM players pl
 JOIN platforms pt on pl.platform_id = pt.id
 WHERE pl.user_id = ${user_id};
--- utils/getPlayerDetails.js
+++ utils/getPlayerDetails.js
@@ -6,9 +6,10 @@ const findRoleByKey = (arr, key) => arr.find((e) => e.role === key).level
 module.exports = async (name, platformId) => {
   const platform = derivePlatform(platformId)
   try {
-    const details = await axios.get(
+    const { data: details } = await axios.get(
       `https://ow-api.com/v1/stats/${platform}/us/${name}/profile`
     )
+
     if (details.private) {
       return {
         portrait: details.icon,
@@ -19,15 +20,27 @@ module.exports = async (name, platformId) => {
       }
     } else {
       const { ratings } = details
-      return {
-        portrait: details.icon,
-        tank_sr: findRoleByKey(ratings, 'tank'),
-        damage_sr: findRoleByKey(ratings, 'damage'),
-        support_sr: findRoleByKey(ratings, 'support'),
-        private: false,
+
+      if (ratings) {
+        return {
+          portrait: details.icon,
+          tank_sr: findRoleByKey(ratings, 'tank'),
+          damage_sr: findRoleByKey(ratings, 'damage'),
+          support_sr: findRoleByKey(ratings, 'support'),
+          private: false,
+        }
+      } else {
+        return {
+          portrait: details.icon,
+          tank_sr: null,
+          damage_sr: null,
+          support_sr: null,
+          private: false,
+        }
       }
     }
   } catch (error) {
+    console.log(error)
     return {
       portrait: null,
       tank_sr: null,
