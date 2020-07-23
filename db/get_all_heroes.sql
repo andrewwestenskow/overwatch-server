a5950bac8106d8747b124bb8f444ec42c56fee22
Andrew Westenskow <andrew.westenskow@gmail.com>
2020-07-23 17:35:57 -0600

    YOOOOO

--- db/get_all_heroes.sql
+++ db/get_all_heroes.sql
@@ -1,2 +1,2 @@
-SELECT h.id, h.name, h.image, r.name AS "role", r.image AS role_image FROM heroes h
+SELECT h.id, h.name, h.image, r.name AS "role", r.id AS role_id, r.image AS role_image FROM heroes h
 JOIN hero_roles r ON h.hero_role_id = r.id
--- db/get_all_maps.sql
+++ db/get_all_maps.sql
@@ -1,2 +1,2 @@
-SELECT m.id, m.name, m.image, gm.name AS game_mode FROM maps m
+SELECT m.id, m.name, m.game_mode_id, m.image, gm.name AS game_mode FROM maps m
 JOIN game_modes gm ON m.game_mode_id = gm.id;
