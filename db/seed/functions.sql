8f5a53c8e42d09f8ed1f2a569fc6002b929334b9
Andrew Westenskow <andrew.westenskow@gmail.com>
2020-11-25 18:37:18 -0500

    Fixed function

--- db/seed/functions.sql
+++ db/seed/functions.sql
@@ -1,7 +1,7 @@
 -- args: player_id, hero_id
 -- GET THE BEST MAP FOR A GIVEN HERO
 CREATE OR REPLACE FUNCTION get_hero_best_map(integer, integer) RETURNS jsonb AS $$
-    SELECT row_to_json(t)::jsonb FROM(SELECT m.id, m.name, m.image, COUNT(win) filter(WHERE "win") AS win_count, COUNT(pr.id) as games_played FROM player_results pr
+    SELECT row_to_json(t)::jsonb FROM(SELECT m.id, m.name, m.image, COUNT(win) filter(WHERE "win") AS win_count, COUNT(win) filter (WHERE NOT "win") as lose_count, COUNT(pr.id) as games_played FROM player_results pr
 JOIN player_results_heroes prh on prh.player_results_id = pr.id 
 JOIN maps m ON pr.map_id = m.id
 WHERE player_id = $1 AND hero_id = $2
@@ -13,7 +13,7 @@ $$ LANGUAGE SQL;
 --args: player_id, hero_id
 -- GET THE WORST MAP FOR A GIVEN HERO
 CREATE OR REPLACE FUNCTION get_hero_worst_map(integer, integer) RETURNS jsonb AS $$
-    SELECT row_to_json(t)::jsonb FROM(SELECT m.id, m.name, m.image, COUNT(win) filter(WHERE "win") AS win_count, COUNT(pr.id) as games_played FROM player_results pr
+    SELECT row_to_json(t)::jsonb FROM(SELECT m.id, m.name, m.image, COUNT(win) filter(WHERE "win") AS win_count, COUNT(win) filter (WHERE NOT "win") as lose_count, COUNT(pr.id) as games_played FROM player_results pr
 JOIN player_results_heroes prh on prh.player_results_id = pr.id 
 JOIN maps m ON pr.map_id = m.id
 WHERE player_id = $1 AND hero_id = $2
